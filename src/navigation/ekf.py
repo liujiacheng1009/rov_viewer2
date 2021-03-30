2816aae43873e9020b5f08b184fa2aa70c9c1fe4
jc <2904152204@qq.com>
2021-03-30 22:49:30 +0800

    add navigation

---
+++ src/navigation/ekf.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import numpy as np 
+import math
+import nvector as nv
+import cv2 
+
+deg = math.pi/180. 
+G = 9.81007
+EPSILON = 1e-6
+
+def skew_matrix(v):
+    return np.array([[0., -v[2], v[1]],[v[2], 0., -v[0]],[-v[1], v[0], 0.]])
+
+class State:
+    def __init__(self):
+        self.timestamp = 0.
+        self.cov = np.zeros((15,15))
+        self.p_GI = np.array([.0,.0,.0])
+        self.v_GI = np.array([.0,.0,.0])
+        self.r_GI = np.eye(3)
+        self.acc_bias = np.array([.0,.0,.0])
+        self.gyr_bias = np.array([.0,.0,.0])
+
+
+class EKF:
+    def __init__(self,acc_n,gyr_n,acc_w,gyr_w):
+        self.acc_noise_ = acc_n
+        self.gyr_noise_ = gyr_n
+        self.acc_bias_noise_ = acc_w
+        self.gyr_bias_noise_ = gyr_w
+        self.state = State()
+
+        sigma_rp = 10*deg 
+        sigma_yaw = 100*deg
+
+        self.state.cov[:3,:3] = np.eye(3)*100
+        self.state.cov[3:6,3:6] = np.eye(3)*100
+        self.state.cov[6:8,6:8] = np.eye(2)*sigma_rp*sigma_rp
+        self.state.cov[8,8] = sigma_yaw * sigma_yaw
+        self.state.cov[9:12,9:12] = np.eye(3) * 0.0004
+        self.state.cov[12:15,12:15] = np.eye(3)*0.0004
+
+    def predict(self, last_imu, curr_imu):
+        dt = curr_imu.timestamp - last_imu.timestamp
+        dt2 = dt*dt 
+        
+        last_state = self.state
+        self.state.timestamp = curr_imu.timestamp
+
+        acc_unbias = 0.5 * (last_imu.acc+curr_imu.acc) - last_state.acc_bias
+        gyr_unbias = 0.5 * (last_imu.gyr + curr_imu.gyr) - last_state.gyr_bias
+        acc_nominal = last_state.r_GI.dot(acc_unbias) + np.array([0,0,-G])
+        self.state.p_GI = last_state.p_GI + last_state.v_GI*dt + 0.5*acc_nominal*dt2
+        self.state.v_GI = last_state.v_GI + acc_nominal * dt
+        delta_angle_axis = gyr_unbias*dt 
+        norm_delta_angle = np.linalg.norm(delta_angle_axis)
+        dR = np.eye(3)
+        if(norm_delta_angle>EPSILON):
+            dR = cv2.Rodrigues(delta_angle_axis)[0]
+            self.state.r_GI = last_state.r_GI.dot(dR)
+
+        Fx = np.eye(15)
+        Fx[:3,:3] = np.eye(3)*dt
+        Fx[3:6,6:9] = -self.state.r_GI.dot(skew_matrix(acc_unbias))*dt 
+        Fx[3:6,9:12] = -self.state.r_GI * dt 
+        if(norm_delta_angle>EPSILON):
+            Fx[6:9,6:9] = dR.T 
+        else:
+            Fx[6:9,6:9] = np.eye(3)
+        Fx[6:9,12:15] = -np.eye(3)*dt 
+
+        Fi = np.zeros((15,12))
+        Fi[3:15,0:12] = np.eye(12)
+
+        Qi = np.zeros((12,12))
+        Qi[:3,:3] = np.eye(3)*dt2*self.acc_noise_
+        Qi[3:6,3:6] = np.eye(3)*dt2*self.gyr_noise_
+        Qi[6:9,6:9] = np.eye(3)*dt*self.acc_bias_noise_
+        Qi[9:12,9:12] = np.eye(3)*dt*self.gyr_bias_noise_
+        self.state.cov = Fx.dot(last_state.cov).dot(Fx.T) + Fi.dot(Qi).dot(Fi.T)
+
+    def update_measurement(self,H,V,r):
+        P = self.state.cov 
+        S = H.dot(P).dot(H.T) + V 
+        K = P.dot(H.T).dot(np.linalg.inv(S))
+
+        delta_x = K.dot(r)
+        self.state.p_GI += delta_x[:3]
+        self.state.v_GI += delta_x[3:6]
+        dR = delta_x[6:9]
+        if(np.linalg.norm(dR)>EPSILON):
+            self.state.r_GI = self.state.r_GI.dot(cv2.Rodrigues(dR)[0])
+        self.state.acc_bias += delta_x[9:12]
+        self.state.gyr_bias += delta_x[12:15]
+        I_KH = np.eye(15) - K.dot(H)
+        self.state.cov = I_KH.dot(P).dot(I_KH.T) + K.dot(V).dot(K.T)
+
+    
+    
+
+
+
+
+    
---
+++ src/navigation/fusion.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+import rospy
+from ekf import EKF,skew_matrix
+from sensor_msgs.msg import Imu, NavSatFix
+from nav_msgs.msg import Odometry,Path
+from geometry_msgs.msg import PoseStamped,Point, Pose, Quaternion, Twist, Vector3
+import numpy as np 
+import nvector as nv
+from collections import deque
+
+
+fixed_id = "world"
+
+class ImuData:
+    def __init__(self):
+        self.timestamp = 0.0
+        self.acc = np.array([.0,.0,.0])
+        self.gyr = np.array([.0,.0,.0])
+
+class GPSData:
+    def __init__(self):
+        self.timestamp = 0.0
+        self.lla = np.array([.0,.0,.0])
+        self.cov = np.eye(3)
+    
+    def lla2enu(self,init_lla,point_lla):
+        n_EA_E = nv.lat_lon2n_E(init_lla[0], init_lla[1])
+        n_EB_E = nv.lat_lon2n_E(point_lla[0], point_lla[1])
+        p_AB_E = nv.n_EA_E_and_n_EB_E2p_AB_E(n_EA_E, n_EB_E, init_lla[2], point_lla[2])
+        R_EN = nv.n_E2R_EN(n_EA_E)
+        p_AB_N = np.dot(R_EN.T, p_AB_E).ravel()
+        return p_AB_N
+
+    def enu2lla(self, init_lla, point_enu, point_lla):
+        pass
+
+
+class Fusion:
+    def __init__(self, acc_n=1e-2,gyr_n=1e-4,acc_w=1e-6,gyr_w=1e-8):
+        self.initialized = False
+        self.init_lla_ = None
+        self.I_p_Gps_ = np.array([.0,.0,.0])
+        self.imu_buff = deque([],maxlen = 100)
+        self.ekf = EKF(acc_n,gyr_n,acc_w,gyr_w)
+        rospy.Subscriber("imu/data", Imu,self.imu_callback)
+        rospy.Subscriber("/fix",NavSatFix, self.gnss_callback)
+        self.pub_path = rospy.Publisher("nav_path",Path,queue_size=10)
+        self.pub_odom = rospy.Publisher("nav_odom",Odometry, queue_size=10)
+        self.nav_path = Path()
+        self.last_imu = None
+
+    def imu_callback(self,msg):
+        imu = ImuData()
+        imu.timestamp = msg.header.stamp.to_sec()
+        imu.acc = np.array([msg.linear_acceleration.x, msg.linear_acceleration.y,
+                    msg.linear_acceleration.z])
+        imu.gyr = np.array([msg.angular_velocity.x, msg.angular_velocity.y,
+                    msg.angular_velocity.z])
+        
+        if(not self.initialized):
+            self.imu_buff.append(imu)
+            return
+        self.ekf.predict(self.last_imu,imu)
+        self.last_imu = imu 
+        self.pub_state()
+
+    def gnss_callback(self,msg):
+        if(msg.status.status != 2):
+            print("Error with GPS!!")
+            return
+        gps = GPSData()
+        gps.timestamp = msg.header.stamp.to_sec()
+        gps.lla = np.array([msg.latitude, msg.longitude, msg.altitude])
+        gps.cov = np.array(msg.position_covariance).reshape((3,3))
+        if(not self.initialized):
+            if(len(self.imu_buff)<100):
+                print("not enough imu data!!")
+                return
+            
+            self.last_imu = self.imu_buff[99]
+            if(abs(gps.timestamp-self.last_imu.timestamp)>0.5):
+                print("GPS and imu not synchronized!!")
+                return
+            self.ekf.state.timestamp = self.last_imu.timestamp
+            if(not self.init_rot_from_imu_data()):
+                return
+            self.init_lla_ = gps.lla
+            self.initialized = True
+            return
+
+        p_G_Gps = gps.lla2enu(self.init_lla_, gps.lla)
+        p_GI = self.ekf.state.p_GI
+        r_GI = self.ekf.state.r_GI
+
+        residual = p_G_Gps-(p_GI+r_GI.dot(self.I_p_Gps_))
+        H = np.zeros((3,15))
+        H[:3,:3] = np.eye(3)
+        H[:3,6:9] =- r_GI.dot(skew_matrix(self.I_p_Gps_))
+        V = gps.cov
+        self.ekf.update_measurement(H,V,residual)
+
+    def init_rot_from_imu_data(self):
+        ## 初始化姿态，加速的方差不能太大
+        sum_acc = np.array([0.,0.,0.])
+        for imu_data in self.imu_buff:
+            sum_acc += imu_data.acc
+        mean_acc = sum_acc /len(self.imu_buff)
+        sum_err2 = np.array([0.,0.,0.])
+        for imu_data in self.imu_buff:
+            sum_err2 += np.power(imu_data.acc-mean_acc,2)
+        std_acc = np.power(sum_err2/len(self.imu_buff),0.5)
+        print(std_acc,mean_acc)
+        if(np.max(std_acc)>3.0):
+            print("acc std is too big !!")
+            return False
+        #import ipdb; ipdb.set_trace()
+        ## 这里获得旋转矩阵的原理是？
+        z_axis = mean_acc/np.linalg.norm(mean_acc)
+        x_axis = np.array([1,0,0]) - z_axis.dot(z_axis.T)*np.array([1,0,0])
+        x_axis = x_axis/np.linalg.norm(x_axis)
+        y_axis = np.cross(z_axis,x_axis)
+        y_axis = y_axis/np.linalg.norm(y_axis)
+
+        r_IG = np.zeros((3,3))
+        r_IG[:1,:3] = x_axis
+        r_IG[1:2,:3] = y_axis
+        r_IG[2:3,:3] = z_axis
+        self.ekf.state.r_GI = r_IG.T ## 初始化姿态
+
+        return True
+
+    def pub_state(self):
+        odom_msg = Odometry()
+        odom_msg.header.frame_id = fixed_id
+        odom_msg.header.stamp = rospy.Time.now()
+        odom_msg.pose.pose = Pose(Point(self.ekf.state.p_GI[0],self.ekf.state.p_GI[1],
+                                    self.ekf.state.p_GI[2]),Quaternion(0,0,0,1))
+        odom_msg.twist.twist = Twist(Vector3(self.ekf.state.v_GI[0],self.ekf.state.v_GI[1],
+                                    self.ekf.state.v_GI[2]), Vector3(0,0,0))
+        self.pub_odom.publish(odom_msg)
+
+        pose_msg = PoseStamped()
+        pose_msg.header = odom_msg.header
+        pose_msg.pose = odom_msg.pose.pose
+        self.nav_path.header = pose_msg.header
+        self.nav_path.poses.append(pose_msg)
+        self.pub_path.publish(self.nav_path)
+
+if __name__ == "__main__":
+    rospy.init_node("imu_gnss_fusion",anonymous=True)
+    f = Fusion()
+    rospy.spin()
